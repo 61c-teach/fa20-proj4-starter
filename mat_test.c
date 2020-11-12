6bb20e441f92d5fe3b3dc98400992f7e7e68885b
Jie Qiu <jieqiu0630@gmail.com>
2020-11-12 10:39:13 -0800

    Fix alloc_ref_test

--- mat_test.c
+++ mat_test.c
@@ -178,7 +178,8 @@ void alloc_success_test(void) {
 }
 
 void alloc_ref_test(void) {
-    matrix *mat = NULL;
+    matrix *mat1 = NULL;
+    matrix *mat2 = NULL;
     matrix *from = NULL;
     allocate_matrix(&from, 3, 2);
     for (int i = 0; i < 3; i++) {
@@ -187,31 +188,32 @@ void alloc_ref_test(void) {
         }
     }
     /* 2D slice */
-    CU_ASSERT_EQUAL(allocate_matrix_ref(&mat, from, 1, 0, 2, 2), 0);
-    CU_ASSERT_PTR_EQUAL(mat->parent, from);
-    CU_ASSERT_EQUAL(mat->parent->ref_cnt, 2);
-    CU_ASSERT_EQUAL(mat->rows, 2);
-    CU_ASSERT_EQUAL(mat->cols, 2);
+    CU_ASSERT_EQUAL(allocate_matrix_ref(&mat1, from, 1, 0, 2, 2), 0);
+    CU_ASSERT_PTR_EQUAL(mat1->parent, from);
+    CU_ASSERT_EQUAL(mat1->parent->ref_cnt, 2);
+    CU_ASSERT_EQUAL(mat1->rows, 2);
+    CU_ASSERT_EQUAL(mat1->cols, 2);
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            CU_ASSERT_EQUAL(get(mat, i, j), get(from, i + 1, j));
+            CU_ASSERT_EQUAL(get(mat1, i, j), get(from, i + 1, j));
         }
     }
     /* 1D slice */
-    CU_ASSERT_EQUAL(allocate_matrix_ref(&mat, from, 1, 0, 2, 1), 0);
-    CU_ASSERT_PTR_EQUAL(mat->parent, from);
-    CU_ASSERT_EQUAL(mat->parent->ref_cnt, 3);
-    CU_ASSERT_EQUAL(mat->rows, 2);
-    CU_ASSERT_EQUAL(mat->cols, 1);
-    CU_ASSERT_NOT_EQUAL(mat->is_1d, 0);
+    CU_ASSERT_EQUAL(allocate_matrix_ref(&mat2, from, 1, 0, 2, 1), 0);
+    CU_ASSERT_PTR_EQUAL(mat2->parent, from);
+    CU_ASSERT_EQUAL(mat2->parent->ref_cnt, 3);
+    CU_ASSERT_EQUAL(mat2->rows, 2);
+    CU_ASSERT_EQUAL(mat2->cols, 1);
+    CU_ASSERT_NOT_EQUAL(mat2->is_1d, 0);
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 1; j++) {
-            CU_ASSERT_EQUAL(get(mat, i, j), get(from, i + 1, j));
+            CU_ASSERT_EQUAL(get(mat2, i, j), get(from, i + 1, j));
         }
     }
     /* Now we compare the data in the reference matrix */
     deallocate_matrix(from);
-    deallocate_matrix(mat);
+    deallocate_matrix(mat1);
+    deallocate_matrix(mat2);
 }
 
 /* Test the null case doesn't crash */
